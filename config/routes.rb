c357c8fda651850b5d9afe452796349b7bbec7dd
Oumaima Afoulous <oumaima.afoulous@yahoo.com>
2022-08-30 02:32:37 +0100

    oumaima's work on : controllers, views models config

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :products do
+  resources :orders, except: :index
+  # We added except: :index because we can access orders directly from the home page.
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,43 @@
+require "test_helper"
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get products_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get products_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get products_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get products_show_url
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get products_delete_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get products_update_url
+    assert_response :success
+  end
+
+  test "should get product_params" do
+    get products_product_params_url
+    assert_response :success
+  end
+
+  test "should get product_find_id" do
+    get products_product_find_id_url
+    assert_response :success
+  end
+end
