9771c58d01b6905c9ead0ec83f0a033b9707fe88
Oumaima Afoulous <oumaima.afoulous@yahoo.com>
2022-09-13 22:47:17 +0100

    Added the notification page and worked on different back-end & front-end problems

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
   resources :orders
-  resources :products,except: [:new, :create,:update, :edit] do
+  resources :products,except: [:new,:update, :edit] do
     resources :orders, except: [:index]
   end
+  get"/mes-notifications", to: "products#notifications"
   get "/mes-recettes", to: "products#recipes"
   get "/mes-recettes/new", to: "products#new", as: "new_recipe"
-  post "/mes-recettes/create", to: "products#create", as: "recipes"
   get "/mes-recettes/:id/edit", to: "products#edit", as: "edit_recipe"
-  patch "/mes-recettes/:id", to: "products#update", as: "recipe"
   patch "/products/:id", to: "products#update"
 end
